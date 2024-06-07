b6609549a4afd81e7ef25fb117ad43b65845c61d
Daniel Luca <hey@danielluca.com>
2024-06-07 15:42:35 +0200

    feat: Add new image to the list of constants

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -5,12 +5,10 @@ export const images = [
 		creator: "Jigar Panchal",
 		profileUrl: "https://unsplash.com/de/@brave4_heart",
 	},
-	// {
-	// 	imageUrl:
-	// 		"https://images.unsplash.com/photo-1673032413639-f2ebc0a82c3f?q=80&w=3023&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	// },
-	// {
-	// 	imageUrl:
-	// 		"https://images.unsplash.com/photo-1703758435024-c91738662ab8?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	// },
+	{
+		imageUrl:
+			"https://images.unsplash.com/photo-1715174539960-6b2f5f279ee5?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		creator: "Paris Bilal",
+		profileUrl: "https://unsplash.com/de/@parisbilal",
+	},
 ];
