2d7a376260ee6bd4eeaadee2ee2335254e349059
Daniel Luca <hey@danielluca.com>
2025-02-02 20:39:49 +0100

    refactor: Remove unused time conversion and server time utility functions

--- src/utility/formatTime.ts
+++
@@ -1,9 +0,0 @@
-export const formatTime = (ms: number) => {
-	// format decimal time like 0.0125 to hh:mm:ss
-	const seconds = Math.floor((ms / 1000) % 60)
-	const minutes = Math.floor((ms / (1000 * 60)) % 60)
-	const hours = Math.floor(ms / (1000 * 60 * 60))
-	return `${hours.toString().padStart(2, "0")}:${minutes
-		.toString()
-		.padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
-}
--- src/utility/getServerTime.tsx
+++
@@ -1,8 +0,0 @@
-export const getServerTime = async () => {
-	const response = await fetch(
-		"https://worldtimeapi.org/api/timezone/Europe/Berlin",
-	)
-	const { datetime } = await response.json()
-	const time = new Date(datetime).getTime()
-	return time
-}
